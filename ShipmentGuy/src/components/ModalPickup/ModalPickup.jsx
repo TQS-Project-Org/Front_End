cafc52ff61abffbd87f20ad45ebcf4e692477a5a
Marcus Peterson Neves Filho <mpeterson.neves@ua.pt>
2023-05-19 12:57:28 +0100

    Feat - Modal incompleto 2

--- ShipmentGuy/src/components/ModalPickup/ModalPickup.jsx
+++ ShipmentGuy/src/components/ModalPickup/ModalPickup.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Col from 'react-bootstrap/Col';
@@ -6,10 +7,50 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
 export default function ModalPickup(props) {
+  const [oldJson, setOldJson] = useState({});
+  const [newJson, setNewJson] = useState({});
+
+  const [name, setName] = useState(props.user.name);
+  const [address, setAddress] = useState(props.user.address);
+  const [city, setCity] = useState(props.user.city);
+  const [state, setState] = useState(props.user.state);
+  const [zip, setZip] = useState(props.user.zip);
+  const [phone, setPhone] = useState(props.user.phone);
+  const [opens, setOpens] = useState(props.user.opens);
+  const [closes, setCloses] = useState(props.user.closes);
+  const [available, setAvailable] = useState(props.user.available);
+
+
+  useEffect(() => {
+    let jsonTemp = props.user;
+    setOldJson(jsonTemp);
+    setNewJson(jsonTemp);
+    setName(jsonTemp.name);
+    setAddress(jsonTemp.address);
+    setCity(jsonTemp.city);
+    setState(jsonTemp.state);
+    setZip(jsonTemp.zip);
+    setPhone(jsonTemp.phone);
+    setOpens(jsonTemp.opens);
+    setCloses(jsonTemp.closes);
+    setAvailable(jsonTemp.available);
+  }, []);
+
+
+    const handleChange = (e) => {
+      setNewJson({[e.target.name]: e.target.value});
+      console.log("Old Json: ", oldJson);
+      console.log("Name: ", e.target.name);
+      console.log("Value: ", e.target.value);
+    }
+
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Form submitted");
+        console.log(newJson);
         props.onHide();
+        props.onEdit();
     }
 
   return (
@@ -29,12 +70,20 @@ export default function ModalPickup(props) {
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridEmail">
           <Form.Label>Name</Form.Label>
-          <Form.Control placeholder="Manuél Gomez" />
+          <Form.Control
+            placeholder="Manuél Gomez"
+            onChange={handleChange}
+            value={name}
+          />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridPassword">
           <Form.Label>Address</Form.Label>
-          <Form.Control placeholder="Rua dos Bobos, N0" />
+          <Form.Control
+            placeholder="Rua dos Bobos, N0"
+            onChange={handleChange}
+            name="address"
+          />
         </Form.Group>
       </Row>
 
@@ -79,7 +128,7 @@ export default function ModalPickup(props) {
 
         <Form.Group className="" controlId="formGridAddress2">
             <Button onClick={props.onHide}>Close</Button>
-            <Button className='ms-2' type='submit'>Send</Button>
+            <Button className='ms-2' type='submit'>Edit</Button>
         </Form.Group>
     </Form>
     </Modal.Body>
